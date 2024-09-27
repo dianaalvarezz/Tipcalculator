6e122d089af940b4ab35c835dd417fb54564c21d
Diana Alvarez <143985084+dianaalvarezz@users.noreply.github.com>
2024-09-26 22:54:42 -0500

    Create MainActivity.java
    I created a simple mainactivity.java file.

---
+++ MainActivity.java
@@ -0,0 +1,10 @@
+package edu.niu.android.tipcalculator;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
